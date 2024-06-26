86d81710af944d2e49bdb84b99e70e814adf5d61
Kavita Dhangar <pingtokavita@gmail.com>
2024-06-26 13:19:14 +0530

    space count using split and length-1

--- spaceCountAssign.js
+++ spaceCountAssign.js
@@ -1,25 +1,35 @@
-function spaceCount(str) {
-    let spaceCounter = 0;
+// function spaceCount(str) {
+//     let spaceCounter = 0;
 
-    // Loop through each character in the string
-    for (let index = 0; index < str.length; index++) {
-        // If the character is a space, increment the counter
-        if (str[index] === ' ') {
-            spaceCounter++;
-        }
-    }
+// //     // Loop through each character in the string
+//     for (let index = 0; index < str.length; index++) {
+//         // If the character is a space, increment the counter
+//         if (str[index] === ' ') {
+//             spaceCounter++;
+//         }
+//     }
+//   // Return the total number of spaces
+//     return spaceCounter;
 
-    // Return the total number of spaces
-    return spaceCounter;
+
+// }
+
+// // Example usage
+// let exampleString1 = "Revision is the mother of success";
+// let exampleString2 = "Javascript is the language of internet world";
+
+// let count1 = spaceCount(exampleString1);
+// let count2 = spaceCount(exampleString2);
+// console.log(`===============================`);
+// console.log(`The total number of spaces in the string "${exampleString1}" is: ${count1}`);
+// console.log(`===============================`);
+// console.log(`The total number of spaces in the string "${exampleString2}" is: ${count2}`);
+
+
+// spaceCount using split() method
+function spaceCountOfString (str){
+let spacesBetString  = str.split(" ").length-1
+console.log(`spaces between string is ${spacesBetString}`);
 }
-
-// Example usage
-let exampleString1 = "Revision is the mother of success";
-let exampleString2 = "Javascript is the language of internet world";
-
-let count1 = spaceCount(exampleString1);
-let count2 = spaceCount(exampleString2);
-console.log(`===============================`);
-console.log(`The total number of spaces in the string "${exampleString1}" is: ${count1}`);
-console.log(`===============================`);
-console.log(`The total number of spaces in the string "${exampleString2}" is: ${count2}`);
+spaceCountOfString("revision is the mother of success");
+spaceCountOfString("javascript is the language of internet world");
